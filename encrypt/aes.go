397ec3c75b83b92fced7e50c948dbba9bee26bc6
saipao <saipao@micous.com>
2023-10-26 19:08:56 +0800

    feat: 增加 aes cbc 加解密

--- encrypt/aes.go
+++ encrypt/aes.go
@@ -1,6 +1,7 @@
 package encrypt
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -39,9 +40,70 @@ func AESCFBDecrypt(data []byte, key string) ([]byte, error) {
 
 	iv := data[:aes.BlockSize]
 	encrypted := data[aes.BlockSize:]
+	decrypted := make([]byte, len(encrypted))
 
 	stream := cipher.NewCFBDecrypter(block, iv)
-	stream.XORKeyStream(encrypted, encrypted)
+	stream.XORKeyStream(decrypted, encrypted)
+
+	return decrypted, nil
+}
+
+func pkcs5Padding(data []byte, blockSize int) []byte {
+	pl := blockSize - len(data)%blockSize
+	text := bytes.Repeat([]byte{byte(pl)}, pl)
+	data = append(data, text...)
+
+	return data
+}
+
+func pkcs5UnPadding(data []byte) []byte {
+	length := len(data)
+	if length == 0 {
+		return nil
+	}
+
+	unPadding := int(data[length-1])
+	return data[:(length - unPadding)]
+}
+
+func AESCBCEncrypt(data []byte, key string) ([]byte, error) {
+	lk := len(key)
+	if lk != 16 && lk != 24 && lk != 32 {
+		return nil, fmt.Errorf("aes cbc key length must be 16 or 24 or 32")
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	data = pkcs5Padding(data, block.BlockSize())
+	iv := []byte(key[:block.BlockSize()])
+	encrypted := make([]byte, len(data))
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(encrypted, data)
 
 	return encrypted, nil
+}
+
+func AESCBCDecrypt(data []byte, key string) ([]byte, error) {
+	lk := len(key)
+	if lk != 16 && lk != 24 && lk != 32 {
+		return nil, fmt.Errorf("aes cbc key length must be 16 or 24 or 32")
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	iv := []byte(key[:block.BlockSize()])
+	decrypted := make([]byte, len(data))
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(decrypted, data)
+
+	decrypted = pkcs5UnPadding(decrypted)
+	return decrypted, nil
 }
--- encrypt/aes_test.go
+++ encrypt/aes_test.go
@@ -23,3 +23,20 @@ func TestAESCFB(t *testing.T) {
 	fmt.Println(string(dd))
 	fmt.Println(string(dd) == data)
 }
+
+func TestAESCBC(t *testing.T) {
+	data := "11csc 测试  aes 222"
+
+	ed, err := AESCBCEncrypt([]byte(data), aesKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dd, err := AESCBCDecrypt(ed, aesKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(dd))
+	fmt.Println(string(dd) == data)
+}
